306fcef2bd057ad466a67617ba759cafd002e9de
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-09-17 16:09:01 -0400

    Create sw.js

---
+++ matematicas/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'matematicas-v1';
+const ASSETS = [
+  './',
+  './operaciones.html',
+  './operaciones_logo.png',
+  './matboy.png',
+  './boyhappy.png',
+  './boytriste.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
